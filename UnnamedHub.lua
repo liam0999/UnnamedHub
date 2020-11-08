e9f0fbaed9d30bb41d94771ea48387f85a6b262c
liam0999 <74009289+liam0999@users.noreply.github.com>
2020-11-07 21:47:55 -0500

    Update UnnamedHub.lua

--- UnnamedHub.lua
+++ UnnamedHub.lua
@@ -6,6 +6,27 @@ if isSuccessful then
 	currentgame = info.Name
 end
 
+local StarterGui = game:GetService("StarterGui")
+local function sendSuccess(gameName)
+	StarterGui:SetCore("SendNotification", {
+		Title = "Unnamed Hub";
+		Text = "Successfully loaded Unnamed Hub for: "..gameName;
+		Duration = "5"
+	})
+	StarterGui:SetCore("SendNotification", {
+		Title = "Unnamed Hub";
+		Text = "Use INSERT to open / close GUI and P to destroy GUI";
+		Duration = "5"
+	})
+end
+local function sendError()
+	StarterGui:SetCore("SendNotification", {
+			Title = "Unnamed Hub";
+			Text = "Error loading Unnamed Hub: Incorrect GameId";
+			Duration = "5"
+		})
+end
+
 local games = {
 	["Odessa, Ukraine"] = {
 		["script"] = "https://raw.githubusercontent.com/liam0999/uhScripts/main/State%20of%20Anarchy%20-%20Odessa-Ukraine";
